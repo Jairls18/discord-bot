8757ac09ac5e4d2d68b010e5d63709c5db60768a
Jair Loayza <jairls1894@gmail.com>
2023-06-05 01:04:43 -0500

    aws

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 const { Client, GatewayIntentBits } = require('discord.js');
 const cron = require('node-cron');
 
+const client = new Client({intents: [GatewayIntentBits.Guilds]})
+
 async function go() {
     const canal = client.channels.cache.get(process.env.CHANNEL_ID); // 👦best-friends
     const rol = canal.guild.roles.cache.get(process.env.ROLE_ID); // @Los Timos (rol)
     canal.send(`Ya pero hoy go o khe? ${rol.toString()}`);
 }
   
-const client = new Client({intents: [GatewayIntentBits.Guilds]})
-
 client.on('ready', () => {
     console.log(`Bot ${client.user.tag} online!`);
     go();
     cron.schedule('00 12 * * *', () => {go();});
-    cron.schedule('00 20 * * *', () => {go();});
+    cron.schedule('00 19 * * *', () => {go();});
 });
 
 client.login(process.env.DISCORD_TOKEN);
