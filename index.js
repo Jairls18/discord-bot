c795fdcf0947beb24d979dcef5dffe5e92375b51
Jair Loayza <jairls1894@gmail.com>
2023-06-05 15:23:00 -0500

    pm2

--- index.js
+++ index.js
@@ -12,10 +12,8 @@ async function go() {
   
 client.on('ready', () => {
     console.log(`Bot ${client.user.tag} online!`);
-    go();
-    cron.schedule('10 13 * * *', () => {console.log('Es hora.');});
-    cron.schedule('10 13 * * *', () => {go();});
-    cron.schedule('00 19 * * *', () => {go();});
+    cron.schedule('00 17 * * *', () => {go();});
+    cron.schedule('00 24 * * *', () => {go();});
 });
 
 client.login(process.env.DISCORD_TOKEN);
